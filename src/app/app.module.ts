c30b799ac6af9bc349f051a79e9a678d524db49c
Alan Cavalcante <alancavalcante307@gmail.com>
2022-10-22 15:06:48 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BarraPesquisaComponent,
-    FooterComponent
+    FooterComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
